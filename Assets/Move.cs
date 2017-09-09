37577a382b2c4f80c943b5dd174accb03a4e9c37
FinaloDaymin <31793590+FinaloDaymin@users.noreply.github.com>
2017-09-09 15:10:54 +0200

    add Move
    Przemek add move

---
+++ Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed, rspeed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update()
+    {
+        transform.Rotate( 0f, 0f, Input.GetAxis( "Horizontal" )*rspeed );
+    }
+
+    void FixedUpdate () {
+        if( Input.GetButton( "Forward" ) )
+        {
+            rb.AddForce( transform.up*speed );
+        }
+    }
+}
